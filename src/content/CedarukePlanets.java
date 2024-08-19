9cd737d854ed064ce7f6bab62f36bf1046eb502e
Xeronaus Finezero <111482467+Xeronaus@users.noreply.github.com>
2024-08-19 21:13:50 +0700

    Create CedarukePlanets.java

---
+++ src/content/CedarukePlanets.java
@@ -0,0 +1,11 @@
+package cedaruke.content;
+
+import arc.graphics.*;
+import mindustry.type.*;
+import mindustry.ctype.*;
+import mindustry.content.*;
+import mindustry.graphics.*;
+import mindustry.graphics.g3d.*;
+import cedaruke.planets.*;
+
+// Needs update
