428a7ff30106667910ab9bb7c15a17ad80c199b6
Xeronaus Finezero <111482467+Xeronaus@users.noreply.github.com>
2024-08-21 11:37:23 +0700

    Update CedarukePlanets.java

--- src/cedaruke/content/CedarukePlanets.java
+++ src/cedaruke/content/CedarukePlanets.java
@@ -1,2 +1,3 @@
 // Add star: Arion
 // Add planets: Georathe, Crajuras, Miranthi, Khupark, Xerghis, Exolarure, Burgharin, Kyrapure, and Alkolaneth
+// When to start? I don't know, if I have free time I guess?
