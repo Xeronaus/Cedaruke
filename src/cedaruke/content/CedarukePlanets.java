aeab3c4bfe8b7e65d1e091f770e0af029daf7620
Xeronaus Finezero <111482467+Xeronaus@users.noreply.github.com>
2024-08-22 07:45:58 +0700

    Update CedarukePlanets.java

--- src/cedaruke/content/CedarukePlanets.java
+++ src/cedaruke/content/CedarukePlanets.java
@@ -2,3 +2,10 @@
 // Add planets: Georathe, Crajuras, Miranthi, Khupark, Xerghis, Exolarure, Burgharin, Kyrapure, and Alkolaneth
 
 package cedaruke.content;
+
+import arc.graphics.*;
+import mindustry.content.*;
+import mindustry.game.*;
+import mindustry.graphics.*;
+import mindustry.graphics.g3d.*;
+import mindustry.type.*;
